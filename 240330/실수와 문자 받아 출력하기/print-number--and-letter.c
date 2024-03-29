2025a3b4ec7fdf951744aebac89e9c041e99b06b
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-03-30 00:18:01 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240330/실수와 문자 받아 출력하기/print-number--and-letter.c
+++ 240330/실수와 문자 받아 출력하기/print-number--and-letter.c
@@ -4,7 +4,7 @@ int main() {
     char c;
     float a,b;
     scanf("%c\n%f\n%f",&c,&a,&b);
-    printf("%c\n%.2f\n%f",c,a,b);
+    printf("%c\n%.2f\n%.2f",c,a,b);
     // 여기에 코드를 작성해주세요.
     return 0;
 }

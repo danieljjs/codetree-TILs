d2e34b6a88148efff9bc922163e291fc133e1946
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-03-29 23:02:30 +0900

    Title: 입력받은 값 교체하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240329/입력받은 값 교체하기/changing-inputs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,temp;
+    scanf("%d %d",&a,&b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("%d %d",a,b);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

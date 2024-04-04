e226c84f48800b5b98dc6b8a6f70b18067b208c0
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-04-04 23:28:02 +0900

    Title: 숫자의 조건 여부 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/숫자의 조건 여부 2/number's-condition-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a==5)
+        printf("A");
+    else if(a%2==0)
+        printf("B");
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

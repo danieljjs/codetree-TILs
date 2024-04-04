d0689de3bb2bc9acbf70daefa74c2565bb81d2f9
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-04-04 23:25:41 +0900

    Title: 최대 2번의 연산, Time: 26ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/최대 2번의 연산/up-to-2-calculations.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a%2==0)
+        a /= 2;
+    if(a%2==1)
+        a = (a+1)/2;
+
+    
+    printf("%d",a);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

0ab64bfecda3c6c766279a6a6e8c3ae9aeecf989
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-04-04 22:43:00 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/시력 검사 2/eye-test-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float a;
+    scanf("%f", &a);
+    if(a>=1.0){
+        printf("High");
+    }else if(a>=0.5){
+        printf("Middle");
+    }else{
+        printf("Low");
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

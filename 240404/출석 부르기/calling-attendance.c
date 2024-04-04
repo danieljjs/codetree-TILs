ffa1a4a737352a7549b7c51663d05b9ba0dced67
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-04-04 23:12:36 +0900

    Title: 출석 부르기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/출석 부르기/calling-attendance.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n==1){
+        printf("John");
+    }else if(n==2){
+        printf("Tom");
+    }else if(n==3){
+        printf("Paul");
+    }else{
+        printf("Vacancy");
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

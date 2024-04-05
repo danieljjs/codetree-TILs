53a7b62457c5b558f95a336e9978656a8208fe36
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-04-06 01:00:23 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/3 또는 5의 배수/multiples-of-3-or-5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a%3==0)
+        printf("YES\n");
+    else
+        printf("NO\n"); 
+    if(a%5==0) 
+        printf("YES");
+    else
+        printf("NO");
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

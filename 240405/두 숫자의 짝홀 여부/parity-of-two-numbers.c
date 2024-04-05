e0f37ebcfa237b36d273c4caf61ecc066b3f4d7e
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-04-05 23:27:04 +0900

    Title: 두 숫자의 짝홀 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/두 숫자의 짝홀 여부/parity-of-two-numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a%2==0)
+        printf("even\n");
+    else
+        printf("odd\n");   
+    if(b%2==0)
+        printf("even");
+    else
+        printf("odd");
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

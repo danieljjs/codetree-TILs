6f3d20427459e974836369adee1d3c604ef6a085
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-04-06 21:22:57 +0900

    Title: 좀 더 어려운 수학 점수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240406/좀 더 어려운 수학 점수/math-scores-are-more-difficult.c
+++ 240406/좀 더 어려운 수학 점수/math-scores-are-more-difficult.c
@@ -2,7 +2,7 @@
 
 int main() {
     int ma, ea, mb, eb;
-    scanf("%d %d\n%%d %d",&ma,&ea,&mb,&eb);
+    scanf("%d %d\n%d %d",&ma,&ea,&mb,&eb);
     if (ma>mb || (ma==mb && ea>eb))
         printf("A");
     else

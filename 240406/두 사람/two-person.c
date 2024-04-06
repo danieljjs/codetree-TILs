bbbd7506d306ef1c3b225ebc7b7754ae906dcd10
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-04-06 22:26:05 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240406/두 사람/two-person.c
+++ 240406/두 사람/two-person.c
@@ -4,7 +4,7 @@ int main() {
     int age1,age2;
     char gender1,gender2;
     scanf("%d %c\n%d %c",&age1,&gender1,&age2,&gender2);
-    if (age1>=19||age2>=19)
+    if ((age1>=19&&gender1=='M')||(age2>=19&&gender2=='M'))
         printf("1");
     else
         printf("0");

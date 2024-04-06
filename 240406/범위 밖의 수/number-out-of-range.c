9d392c4f2dcb61af63fa81ffa89871e761c9fbe3
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-04-06 11:48:43 +0900

    Title: 범위 밖의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/범위 밖의 수/number-out-of-range.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a<10 || a>20)
+        printf("yes");
+    else
+        printf("no");    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

53cfb28206cbce0dc7241c65b8392c432c329b27
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-04-06 16:10:41 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240406/좀 더 어려운 수학 점수/math-scores-are-more-difficult.c
+++ 240406/좀 더 어려운 수학 점수/math-scores-are-more-difficult.c
@@ -3,12 +3,8 @@
 int main() {
     int ma, ea, mb, eb;
     scanf("%d %d\n%%d %d",&ma,&ea,&mb,&eb);
-    if (ma>mb)
+    if (ma>mb || ma==mb && ea>eb)
         printf("A");
-    else if (ma==mb && ea>eb)
-        printf("A");
-    else if (ma==mb && ea<eb)
-        printf("B");
     else
         printf("B");
     // 여기에 코드를 작성해주세요.

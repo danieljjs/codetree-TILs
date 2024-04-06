1fa4ae91eb968f849baa6571f46d625732985d34
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-04-07 03:18:55 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240407/코로나 메뉴얼/covid-manual.c
+++ 240407/코로나 메뉴얼/covid-manual.c
@@ -3,7 +3,7 @@
 int main() {
     int tem1,tem2,tem3;
     char sym1,sym2,sym3;
-    scanf("%c %d\n%c %d\n%c %d",&tem1,&sym1,&tem2,&sym2,&tem3,&sym3);
+    scanf("%c %d\n%c %d\n%c %d",&sym1,&tem1,&sym2,&tem2,&sym3,&tem3);
     if (tem1>=37 && sym1=='Y')
         if ((tem2>=37 && sym2=='Y') || (tem3>=37 && sym3=='Y'))
             printf("E");

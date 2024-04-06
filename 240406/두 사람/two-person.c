85fccdcf94f5b3c188f3fba9b21fc48ca305b591
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-04-06 22:23:14 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240406/두 사람/two-person.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age1,age2;
+    char gender1,gender2;
+    scanf("%d %c\n%d %c",&age1,&gender1,&age2,&gender2);
+    if (age1>=19||age2>=19)
+        printf("1");
+    else
+        printf("0");
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

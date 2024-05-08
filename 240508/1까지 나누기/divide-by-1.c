16efff739437dc45d102c99478d51b25d8e4c274
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-05-08 12:49:21 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240508/1까지 나누기/divide-by-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+	int cnt = 0;
+    scanf("%d",&n);
+	for (int i = 1; i <= 100; i++) {
+        n/=i;
+        cnt++;
+		if (n <= 1) {
+            printf("%d", cnt);
+			break;
+		}
+	}
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

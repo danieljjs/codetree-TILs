0ca2d23ab20c8193bc50e2a0a36483f0d9c040a4
danieljjs <164819526+danieljjs@users.noreply.github.com>
2024-05-08 17:16:27 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240508/조건에 따른 연산/operatino-by-rule.c
+++ 240508/조건에 따른 연산/operatino-by-rule.c
@@ -5,14 +5,14 @@ int main() {
 	scanf("%d", &n);
 
 	while (1) {
+        if(n >= 1000)
+            break;
 		if (n % 2 == 0) {
 			n = n * 3 + 1;
 		}
 		else {
 			n = n * 2 + 2;
 		}
-        if(n >= 1000)
-            break;
         cnt++;
 	}
     printf("%d",cnt);
